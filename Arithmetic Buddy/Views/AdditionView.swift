a36613cf4118f565a811464c219aa63ee3b111be
Lexi Harrop <lharrop@lcs.on.ca>
2024-02-03 17:29:46 -0500

    Created the project and finished initial project organization.

---
+++ Arithmetic Buddy/Views/AdditionView.swift
@@ -0,0 +1,18 @@
+//
+//  AdditionView.swift
+//  Arithmetic Buddy
+//
+//  Created by Lexi on 2024-02-03.
+//
+
+import SwiftUI
+
+struct AdditionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdditionView()
+}
